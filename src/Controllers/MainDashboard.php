3811cf5ae9eeb4882fcf388fc9a1bb679614189f
Tom Noogen <tom@tnguyen-imac.local>
2017-03-03 10:23:29 -0600

    multi-single page app

--- src/Controllers/MainDashboard.php
+++ src/Controllers/MainDashboard.php
@@ -2,7 +2,7 @@
 
 namespace Controllers;
 
-class MainDashboard extends BaseSecureController
+class MainDashboard extends BaseController
 {
     public function getDashboard() 
     {
@@ -10,5 +10,6 @@ class MainDashboard extends BaseSecureController
     	// filter out projects user does not have permission to
     	// return and render to main dashboard
     	// render main dashboard
+        $this->render('main.html');
     }
 }
