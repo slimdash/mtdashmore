baad285de6b6ddaff8aa62e396a98f8f67a87b37
Thanh Nguyen <Thanh Nguyen>
2017-03-05 01:19:59 -0600

    updates

--- src/Controllers/ProjectDashboard.php
+++ src/Controllers/ProjectDashboard.php
@@ -7,6 +7,13 @@ class ProjectDashboard extends BaseSecureController
     public function getDashboard() 
     {
     	// if user does not have permission to project, redirect to login
-    	// render project
+    	// get list of modules
+    	// filter out any module that projects does not have permission to
+    	// render dashboard with modules
+    }
+
+    public function getModule() 
+    {
+    	// render module
     }
 }
